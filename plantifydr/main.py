afe532be29c01e113311e8e0ede9998ded3b32e0
Ali Lavaee <lavaalex3@gmail.com>
2021-02-23 17:24:04 -0800

    fixed base64

--- plantifydr/main.py
+++ plantifydr/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from settings import *
 from model import detect
 from fastai.vision.all import *
+import base64
 
 app = Flask(__name__)
 
@@ -12,7 +13,8 @@ def classify():
     print('Starting...')
     data = request.form
     plant_type = data['plant_type']
-    img = PILImage.create(request.files['file'].stream)
+    # print(base64.b64decode(request.files['file'].read())[:10])
+    img = PILImage.create(base64.b64decode(request.files['file'].read()))
     # import pdb; pdb.set_trace()
     print('Detecting...')
     diagnosis = detect(img,plant_type)
